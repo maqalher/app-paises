a3314c00343ced1ebd90f7a8d17ee9ba38d9f0b6
maqalher <maqalher@hotmail.com>
2022-01-27 20:59:29 -0600

    add sugerencias

--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Country } from '../../interfaces/pais.interface';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-por-region',
@@ -6,11 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class PorRegionComponent implements OnInit {
+export class PorRegionComponent{
 
-  constructor() { }
+  regiones: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC'];
+  regionActiva: string = ''
+  paises: Country[] = [];
 
-  ngOnInit(): void {
+  constructor( private paisService:PaisService ) { }
+
+  getClaseCSS(region:string):string {
+    return (region === this.regionActiva ? 'btn btn-primary m-2' : 'btn btn-outline-primary m-2');
   }
 
+  activarRegion(region:string) {
+
+    if(region === this.regionActiva) return;
+
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService.buscarRegion(region)
+      .subscribe( paises => {
+        // console.log(paises);
+        this.paises = paises;
+        }
+      )
+  }
 }
